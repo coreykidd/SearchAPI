172fcf832c72d2fdb498092dcdc384f472efa45d
coreykidd <corey@coreykidd.com>
2019-08-24 15:13:02 -0500

    Added highlights to body. Limited number of chracters returned.

--- SearchAPI/Models/SearchResult.cs
+++ SearchAPI/Models/SearchResult.cs
@@ -8,7 +8,17 @@ namespace SearchAPI.Models
     public class SearchResult
     {
         public string Subject { get; set; }
-        public string Body { get; set; }
         public string ConversationId { get; set; }
+        //public KeyValuePair<string, IReadOnlyCollection<string>> Highlight { get; set; }
+        public string Body { get; set; }
+
+        public SearchResult(string subject, string conversationId, string body)
+        {
+            this.Subject = subject;
+            this.ConversationId = conversationId;
+            this.Body = body;
+        }
+
+        public SearchResult() { }
     }
 }
--- SearchAPI/Repositories/SearchRepository.cs
+++ SearchAPI/Repositories/SearchRepository.cs
@@ -27,13 +27,44 @@ namespace SearchAPI.Repositories
            .Index(index)
            .From(0)
            .Size(10)
+           .Source(src => src
+            .Includes(i => i
+                .Fields(
+                    p => p.Subject,
+                    p => p.ConversationId
+                    )))
             .Query(q => q
                 .QueryString(qs => qs
                     .DefaultField("*")
-                    .Query(query))));
+                    .Query(query)))
+            .Highlight(h => h
+                .PreTags("<mark>")
+                .PostTags("</mark>")
+                .Fields(fs => fs
+                    .Field(f => f.Body)
+                    .NumberOfFragments(1)
+                    .FragmentSize(200)))
+            );
 
-            var enumerable = searchResponse.Hits.Select(h => h.Source);
-            return enumerable;
+            List<SearchResult> searchResults = new List<SearchResult>();
+            //There's almost certainly a way to do this in LINQ, but I can't figure it out at the moment
+            //Needs to deserialize metadata and highlighted body to object in one call
+            foreach (var hit in searchResponse.Hits)
+            {
+                string subject = hit.Source.Subject;
+                string conversationId = hit.Source.ConversationId;
+                string body = "";
+                foreach (var highlightCollection in hit.Highlight)
+                {
+                    foreach(var highlight in highlightCollection.Value)
+                    {
+                        //Right now API only handles a single hit highlight, could easily be expanded later
+                        body = highlight;
+                    }
+                }
+                searchResults.Add(new SearchResult(subject, conversationId, body));
+            }
+            return searchResults;
         }
     }
 }
